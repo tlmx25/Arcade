d323e05774308d541d4acb114884bc7cb48c4547
Tom <tom.lefoix@epitech.eu>
2024-03-20 19:02:20 +0100

    add: ninja debug to build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 if [ "$1" = "--help" ] ||  [ "$1" = "-h" ]; then
-  echo "Usage: ./build.sh [re|debug|ninja]"
-  echo "re: remove build directory and rebuild"
-  echo "debug: build in debug mode"
+  echo "Usage: ./build.sh [debug|ninja] [re]"
   echo "ninja: build using ninja"
+  echo "debug: build in debug mode"
+  echo "re: remove build directory and rebuild"
   exit 0
 fi
 
@@ -10,6 +10,12 @@ if [ "$1" = "re"  ]; then
   rm -rf build
   rm -rf cmake-build-debug
 fi
+
+if [ "$2" = "re"  ]; then
+  rm -rf build
+  rm -rf cmake-build-debug
+fi
+
 
 if [ "$1" = "debug" ]; then
   echo "Debug mode"
@@ -23,7 +29,12 @@ if [ "$1" = "ninja" ]; then
   echo "Ninja mode"
   rm -rf build
   rm -rf cmake-build-debug
-  mkdir -p build && cd build && cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release
+
+  if [ "$2" = "debug" ]; then
+    mkdir -p build && cd build && cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Debug && ninja && cd ..
+    exit 0
+  fi
+  mkdir -p build && cd build && cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release && ninja && cd ..
   exit 0
 fi
 
