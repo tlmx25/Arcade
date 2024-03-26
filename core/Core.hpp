7bae4b716c1e2be6fb0a457078f308a2c47e64ae
Tom <tom.lefoix@epitech.eu>
2024-03-26 14:28:44 +0100

    add: menu for manage game

--- core/Core.hpp
+++ core/Core.hpp
@@ -33,20 +33,25 @@ public:
 
 
 private:
-    std::shared_ptr<Arcade::IDisplay> _display; ///< The display object.
-    std::shared_ptr<Arcade::IGame> _game; ///< The game object.
+    std::unique_ptr<Arcade::IDisplay> _display; ///< The display object.
+    std::unique_ptr<Arcade::IGame> _game; ///< The game object.
     std::string _currentLib; ///< The current graphic library.
     std::string _currentGame; ///< The current game.
-    std::vector<std::string> _libs; ///< The available graphic libraries.
-    std::vector<std::string> _games; ///< The available games.
+    std::vector<std::string> _libsList; ///< The available graphic libraries.
+    std::vector<std::string> _gamesList; ///< The available games.
     bool _isInMenu = true; ///< A boolean to know if the core is in the menu.
     bool _isRunning = false; ///< A boolean to know if the core is running.*
     std::string _username; ///< The username of the player.
     void manageEvent(Arcade::Event event);
+    void manageUsername(Arcade::Event);
     std::vector<std::shared_ptr<Arcade::Object>> menu();
     void refreshLibs();
     void loadAllLibs();
     std::string getLibName(std::string const &path);
+    std::string getNextLib(std::string const &currentLib, std::vector<std::string> const &libs);
+    std::string getPreviousLib(std::string const &currentLib, std::vector<std::string> const &libs);
+    void setGame(int PreviousOrNext);
+    void setDisplay(int PreviousOrNext);
 
     public:
         /**
